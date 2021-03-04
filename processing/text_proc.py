ac680d9e5ab04338ac017c46a391e820812fe229
denis <mistrny@gmail.com>
2021-03-04 10:43:44 +0300

    сплит текста

--- processing/text_proc.py
+++ processing/text_proc.py
@@ -52,7 +52,7 @@ def splitin():
             flag=False
         if flag:
             if len(el)>17:
-                if el[0:16]!='всего соообщений':
+                if el[0:16]!='всего сообщений:':
                     with open(lin_way+'new_part4.txt','a') as f:
                         f.write(el+'. ')
                 else:
@@ -60,7 +60,7 @@ def splitin():
                         f.write(el+'. ')
         else:
             if len(el)>17:
-                if el[0:16]!='всего соообщений':
+                if el[0:16]!='всего сообщений:':
                     with open(lin_way+'part5.txt','a') as f:
                         f.write(el+'. ')
                 else:
