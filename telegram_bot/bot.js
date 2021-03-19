d6de8b138a67f173cbcb1df445e209493f03fcb8
whitewolf185 <whitewolf.mot185@gmail.com>
2021-03-19 18:03:17 +0300

    Починил фотки

--- telegram_bot/bot.js
+++ telegram_bot/bot.js
@@ -2,7 +2,19 @@ const fs = require('fs');
 const TgBot = require('node-telegram-bot-api');
 let token = '';
 
+function OK (msg){
+    // массив проблемных мест
+    let array = ["forward_from_chat", "sticker", "photo"];
+    for (let i = 0; i < array.length; ++i){
+        if(msg[array[i]] !== undefined){
+            return false;
+        }
+    }
+    return true;
+}
+
 let start = new Promise((resolve, reject) => {
+    //изменять слеш, если работает бот на винде
     fs.readFile(__dirname + '/token.txt', 'utf8', (err, data) => {
         if (err) {
             reject(err);
@@ -30,12 +42,28 @@ start
             })
 
             bot.on('message', (msg) => {
+                // console.log(msg);
                 //if нужен, чтобы не обрабатывать пересылаемые сообщения
-                if(msg.forward_from_chat === undefined && commands.indexOf(msg.text) == -1){
+                let haveComands = commands.indexOf(msg.text) == -1;
+                if(OK(msg) && haveComands){
                     let result = '';
                     if(msg.text.split(" ").length > 1){
                         let url = msg.text.match(/\bhttp.+\b/);
                         let tmp = msg.text.toLowerCase();
+                        tmp = tmp.replace(/[!#&\*,;\\^_`{}]/g," ");
+                        tmp = tmp.replace(/\bhttp.+\b/,url);
+                        tmp = tmp.replace(/\s+/g," ");
+                        (tmp[tmp.length - 1] != ".") ? result += tmp + "." : result += tmp;
+                        console.log(result);
+                        fs.appendFileSync(__dirname + '/words.txt', `\n${result}`)
+                    }
+                }
+
+                if(msg.photo !== undefined && haveComands){
+                    let result = '';
+                    if(msg.caption.split(" ").length > 1){
+                        let url = msg.caption.match(/\bhttp.+\b/);
+                        let tmp = msg.caption.toLowerCase();
                         tmp = tmp.replace(/[!#&\*,;\\^_`{}]/g," ");
                         tmp = tmp.replace(/\bhttp.+\b/,url);
                         tmp = tmp.replace(/\s+/g," ");
